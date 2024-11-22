5afcdcfbe36c0f750fb1c581c9d58a6f95b6d9d2
Jason Scott <jpscott07@gmail.com>
2024-11-22 16:09:38 +0000

    extract delay to module, fix include type

--- kernel/2b/src/kernel/kernel.c
+++ kernel/2b/src/kernel/kernel.c
@@ -7,8 +7,9 @@
  * 
  * @copyright Copyright (c) 2024
  */
-#include "stddef.h"
-#include "stdint.h"
+#include <stddef.h>
+#include <stdint.h>
+#include "delay.h"
 
 static inline void mmio_write(uint32_t reg, uint32_t data)
 {
@@ -18,13 +19,6 @@ static inline void mmio_write(uint32_t reg, uint32_t data)
 static inline uint32_t mmio_read(uint32_t reg)
 {
     return *(volatile uint32_t*)reg;
-}
-
-// Loop <delay> times in a way that the compiler won't optimize away.
-static inline void delay(int32_t count)
-{
-    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
-            : "=r"(count): [count]"0"(count) : "cc");
 }
 
 enum
