0bf00071c849439b775edd6837783251112ba71e
Jason Scott <jpscott07@gmail.com>
2024-11-25 14:31:57 +0000

    minimal build config for pi 1

---
+++ projects/raspi1/raspi1.mk
@@ -0,0 +1,33 @@
+##
+# Raspberry Pi 2B Project Build Config
+# 
+
+# Get the directory of this Makefile.
+# 
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+THIS_DIR := $(dir $(THIS_MAKEFILE))
+
+# Set CPU and add project-specific flags.
+# 
+CPU := arm1176jzf-s
+CFLAGS += -DMODEL_1
+
+# Sources.
+# 
+SRCS += $(THIS_DIR)src/common/delay.c
+SRCS += $(THIS_DIR)src/common/stdio.c
+SRCS += $(THIS_DIR)src/common/stdlib.c
+
+SRCS += $(THIS_DIR)src/kernel/kernel.c
+SRCS += $(THIS_DIR)src/kernel/uart.c
+
+SRCS += $(THIS_DIR)src/kernel/boot.S
+
+# Include dirs.
+# 
+INC_DIRS += $(THIS_DIR)src/common
+INC_DIRS += $(THIS_DIR)src/kernel
+
+# Linker script.
+# 
+LD_SCRIPT := $(THIS_DIR)linker.ld
